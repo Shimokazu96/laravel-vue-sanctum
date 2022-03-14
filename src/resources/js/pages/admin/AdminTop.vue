0ac0a4f6b9531c6fef24311580966bd72f5e80b3
1st-shimooka <78331743+ShimookaKazuhiko@users.noreply.github.com>
2022-03-14 13:19:28 +0900

    フロントロジック（修正は必要）

---
+++ src/resources/js/pages/admin/AdminTop.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <AdminNavbar />
+        <main>
+            <div class="container">
+                <RouterView />
+            </div>
+        </main>
+        <AdminFooter />
+    </div>
+</template>
+
+<script>
+import AdminNavbar from "../../components/AdminNavbar.vue";
+import AdminFooter from "../../components/AdminFooter.vue";
+export default {
+    components: {
+        AdminNavbar,
+        AdminFooter,
+    },
+};
+</script>
